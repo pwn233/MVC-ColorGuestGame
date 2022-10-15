005eb5684107e5777b7c1087b77a5eadea0176ba
Anuwatr Treecharoenrat <astiweiei@gmail.com>
2022-10-15 20:14:40 +0700

    First Push

---
+++ src/Control/Control.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import Model.Model;
+
+/**
+ *
+ * @author pwn233
+ */
+public class Control {
+    private String guessInput;
+    Model m = new Model();
+    private String status = "";
+    public void start(String x) {
+        this.guessInput = x;
+    }
+    public void checkGuess(){
+        m.checkParent(guessInput);
+    }
+    public void generateParent() {
+        m.generateParent();
+    }
+    public String returnStatus() {
+       status = m.checkWin();
+       return status;
+    }
+    public void roundUpdate() {
+       m.round();
+    }
+    public String roundCheck() {
+        return m.roundCheck();
+    }
+    public int getRoundCount() {
+        return m.getRoundCount();
+    }
+}
---
+++ src/Model/Model.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Random;
+
+/**
+ *
+ * @author pwn233
+ */
+public class Model {
+    protected char[] parent = new char[6];
+    private char[] colorList = {'R', 'G','Y','B', 'P', 'O'};
+    private char[] gi = new char[6];
+    private String dummy = "";
+    private int roundCount = 1;
+
+    public void generateParent(){
+        Random rd = new Random();
+        for(int i=0; i < 6; i++) {
+           parent[i] = colorList[rd.nextInt(6)];
+           System.out.print(parent[i]);
+        }
+        System.out.println("\nGenerate Parent Complete");
+    }
+    public char[] getParrent() {
+        return parent;
+    }    
+    public void checkParent(String guessInput) {
+        //take input
+        dummy = "";
+        for(int i = 0; i < gi.length; i ++) {
+            gi[i] = guessInput.charAt(i);
+        }
+        //check
+        boolean checkVal = false;
+        for(int i = 0; i < guessInput.length(); i++ ) {
+            if(gi[i] == parent[i]) {
+                dummy  += "B";
+                checkVal = true;
+            } else {
+                for(int j = i+1; j < 6; j++) {
+                    if(gi[i] == parent[j]) {
+                        dummy += "W";
+                        checkVal = true;
+                    }   
+                }
+                if(checkVal == false) {
+                    dummy += "-";
+                }
+            }
+        }
+    }
+    public String checkWin() {
+        String status = "";
+        if(dummy.equals("BBBBBB")) {
+            status = "You win! : "+dummy+" "+roundCheck();
+            generateParent();
+            roundCount = 0;
+        } 
+        else {
+            status = "Not yet, but this might help : "+dummy+" and "+roundCheck();
+        }
+        return status;
+    }
+
+    public void round() {
+        roundCount++;
+    }
+    public String roundCheck() {
+        if(roundCount == 9) {
+          return "Game Over!, out of rounds to play.";
+        } else
+        return "Played "+roundCount+"/8.";   
+    }
+    public int getRoundCount() {
+        return roundCount;
+    }
+}
